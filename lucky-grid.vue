458e59d3740e7aa936f1a8bc00e133f0f6a4f56b
buuing <17778888138@189.cn>
2020-12-05 23:03:39 +0800

    fix: 恢复根目录下导出

---
+++ lucky-grid.vue
@@ -0,0 +1,197 @@
+<template>
+  <view v-if="isShow" class="lucky-box" :style="{ width: boxWidth + 'px', height: boxHeight + 'px' }">
+    <canvas
+      id="lucky-grid"
+      canvas-id="lucky-grid"
+      :style="{ width: boxWidth + 'px', height: boxHeight + 'px' }"
+    ></canvas>
+    <view class="lucky-grid-btn" @click="toPlay" :style="{
+      left: btnLeft + 'px',
+      top: btnTop + 'px',
+      width: btnWidth + 'px',
+      height: btnHeight + 'px',
+    }"></view>
+  </view>
+</template>
+
+<script>
+  import { LuckyGrid } from 'lucky-canvas'
+  export default {
+    name: 'lucky-grid',
+    data () {
+      return {
+        isShow: false,
+        boxWidth: 100,
+        boxHeight: 100,
+        btnWidth: 0,
+        btnHeight: 0,
+        btnLeft: 0,
+        btnTop: 0,
+      }
+    },
+    props: {
+      width: {
+        type: String,
+        default: '600rpx'
+      },
+      height: {
+        type: String,
+        default: '600rpx'
+      },
+      cols: {
+        type: [String, Number],
+        default: 3,
+      },
+      rows: {
+        type: [String, Number],
+        default: 3,
+      },
+      blocks: {
+        type: Array,
+        default: () => []
+      },
+      prizes: {
+        type: Array,
+        default: () => []
+      },
+      button: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      defaultConfig: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      defaultStyle: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      activeStyle: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    watch: {
+      prizes: {
+        handler (newData, oldData) {
+          this.$lucky.prizes = newData
+        },
+        deep: true
+      },
+      button: {
+        handler (newData, oldData) {
+          this.$lucky.button = newData
+        },
+        deep: true
+      }
+    },
+    methods: {
+      init () {
+        this.boxWidth = this.changeUnits(this.width)
+        this.boxHeight = this.changeUnits(this.height)
+        this.isShow = true
+        this.$nextTick(() => {
+          this.draw()
+        })
+      },
+      draw () {
+        const ctx = this.ctx = uni.createCanvasContext('lucky-grid', this)
+        const dpr = this.dpr = uni.getSystemInfoSync().pixelRatio
+        console.log(dpr)
+        const $lucky = this.$lucky = new LuckyGrid({
+          // #ifdef H5
+          flag: 'UNI-H5',
+          // #endif
+          // #ifdef MP
+          flag: 'UNI-MINI-WX',
+          // #endif
+          ctx: ctx,
+          width: this.width,
+          height: this.height,
+          setTimeout: setTimeout,
+          clearTimeout: clearTimeout,
+          setInterval: setInterval,
+          clearInterval: clearInterval,
+        }, {
+          ...this.$props,
+          start: (...rest) => {
+            this.$emit('start', ...rest)
+          },
+          end: (...rest) => {
+            this.$emit('end', ...rest)
+          },
+        })
+        // 动态设置按钮大小
+        const button = this.$props.button
+        if (button) {
+          const [x, y, width, height] = $lucky.getGeometricProperty([
+            button.x,
+            button.y,
+            button.col || 1,
+            button.row || 1
+          ])
+          console.log([x, y, width, height])
+          this.btnLeft = x
+          this.btnTop = y
+          this.btnWidth = width
+          this.btnHeight = height
+        }
+      },
+      toPlay (e) {
+        this.$lucky.startCallback()
+      },
+      play (...rest) {
+        this.$lucky.play(...rest)
+      },
+      stop (...rest) {
+        this.$lucky.stop(...rest)
+      },
+      rpx2px (value) {
+        if (typeof value === 'string') value = Number(value.replace(/[a-z]*/g, ''))
+        return uni.getSystemInfoSync().windowWidth / 750 * value
+      },
+      changeUnits (value) {
+        return Number(value.replace(/^(\-*[0-9.]*)([a-z%]*)$/, (value, num, unit) => {
+          switch (unit) {
+            case 'px':
+              num *= 1
+              break
+            case 'rpx':
+              num = this.rpx2px(num)
+              break
+            default:
+              num *= 1
+              break
+          }
+          return num
+        }))
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .lucky-box {
+    position: relative;
+    overflow: hidden;
+  }
+  .lucky-box canvas {
+    position: absolute;
+  }
+  .lucky-grid-btn {
+    position: absolute;
+    background: rgba(0, 0, 0, 0);
+    border-radius: 0;
+  }
+</style>
---
+++ lucky-wheel.vue
@@ -0,0 +1,166 @@
+<template>
+  <view v-if="isShow" class="lucky-box" :style="{ width: boxWidth + 'px', height: boxHeight + 'px' }">
+    <canvas
+      id="lucky-wheel"
+      canvas-id="lucky-wheel"
+      :style="{ width: boxWidth + 'px', height: boxHeight + 'px' }"
+    ></canvas>
+    <view class="lucky-wheel-btn" @click="toPlay" :style="{
+      width: btnWidth + 'px',
+      height: btnHeight + 'px',
+    }"></view>
+  </view>
+</template>
+
+<script>
+  import { LuckyWheel } from 'lucky-canvas'
+  export default {
+    name: 'lucky-wheel',
+    data () {
+      return {
+        isShow: false,
+        boxWidth: 100,
+        boxHeight: 100,
+        btnWidth: 0,
+        btnHeight: 0,
+      }
+    },
+    props: {
+      width: {
+        type: String,
+        default: '600rpx'
+      },
+      height: {
+        type: String,
+        default: '600rpx'
+      },
+      blocks: {
+        type: Array,
+        default: () => []
+      },
+      prizes: {
+        type: Array,
+        default: () => []
+      },
+      buttons: {
+        type: Array,
+        default: () => []
+      },
+      defaultConfig: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      defaultStyle: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+    },
+    mounted () {
+      this.init()
+    },
+    watch: {
+      prizes: {
+        handler (newData, oldData) {
+          this.$lucky.prizes = newData
+        },
+        deep: true
+      },
+      buttons: {
+        handler (newData, oldData) {
+          this.$lucky.buttons = newData
+        },
+        deep: true
+      }
+    },
+    methods: {
+      init () {
+        this.boxWidth = this.changeUnits(this.width)
+        this.boxHeight = this.changeUnits(this.height)
+        this.isShow = true
+        this.$nextTick(() => {
+          this.draw()
+        })
+      },
+      draw () {
+        const ctx = this.ctx = uni.createCanvasContext('lucky-wheel', this)
+        const dpr = this.dpr = uni.getSystemInfoSync().pixelRatio
+        const $lucky = this.$lucky = new LuckyWheel({
+          // #ifdef H5
+          flag: 'UNI-H5',
+          // #endif
+          // #ifdef MP
+          flag: 'UNI-MINI-WX',
+          // #endif
+          ctx: ctx,
+          width: this.width,
+          height: this.height,
+          setTimeout: setTimeout,
+          clearTimeout: clearTimeout,
+          setInterval: setInterval,
+          clearInterval: clearInterval,
+        }, {
+          ...this.$props,
+          start: (...rest) => {
+            this.$emit('start', ...rest)
+          },
+          end: (...rest) => {
+            this.$emit('end', ...rest)
+          },
+        })
+        // 动态设置按钮大小
+        this.btnWidth = $lucky.maxBtnRadius * 2
+        this.btnHeight = $lucky.maxBtnRadius * 2
+      },
+      toPlay (e) {
+        this.$lucky.startCallback()
+      },
+      play (...rest) {
+        this.$lucky.play(...rest)
+      },
+      stop (...rest) {
+        this.$lucky.stop(...rest)
+      },
+      rpx2px (value) {
+        if (typeof value === 'string') value = Number(value.replace(/[a-z]*/g, ''))
+        return uni.getSystemInfoSync().windowWidth / 750 * value
+      },
+      changeUnits (value) {
+        return Number(value.replace(/^(\-*[0-9.]*)([a-z%]*)$/, (value, num, unit) => {
+          switch (unit) {
+            case 'px':
+              num *= 1
+              break
+            case 'rpx':
+              num = this.rpx2px(num)
+              break
+            default:
+              num *= 1
+              break
+          }
+          return num
+        }))
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .lucky-box {
+    position: relative;
+  }
+  .lucky-box canvas {
+    position: absolute;
+  }
+  .lucky-wheel-btn {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background: rgba(0, 0, 0, 0);
+    border-radius: 50%;
+  }
+</style>
